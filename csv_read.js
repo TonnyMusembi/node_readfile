adf72cd0bc6557b4196c37d748e711badce62292
TonnyMusembi <musembutonnyj@gmail.com>
2023-02-10 10:20:29 +0300

    update

--- csv_read.js
+++ csv_read.js
@@ -1,15 +1,20 @@
 const fs = require("fs");
 const csv = require("csv-parser");
+let sum = 0;
+const selectedFields = ["name", "age", "email"];
 
 const results = [];
 fs.createReadStream("file.csv")
     .pipe(csv())
     .on(
         "data",
+        // (row) => {
+        //     sum += parseInt(row);
+        // }
         (data) => results.push(data)
-        // console.log(data);
     )
-    .on("end", () => {
-        // console.log("CSV file successfully processed");
-        console.log(results);
-    });
+
+.on("end", () => {
+    console.log("CSV file successfully processed");
+    console.log(results);
+});
